0d76a07b12531c00c16cd5d559e320949820ead4
Richa <richasahu4190@gmail.com>
2025-08-04 15:39:14 +0530

    new commit

--- src/main/java/Package/Lab11.java
+++ src/main/java/Package/Lab11.java
@@ -8,18 +8,14 @@ public class Lab11 {
 	    public static void main(String[] args) {
 	        Scanner sc = new Scanner(System.in);
 
-	        // Accept input from user
 	        System.out.print("Enter date (yyyy-mm-dd): ");
 	        String input = sc.nextLine();
 
-	        // Parse the entered date
 	        LocalDate enteredDate = LocalDate.parse(input);
 	        LocalDate currentDate = LocalDate.now();
 
-	        // Calculate duration
 	        Period duration = Period.between(enteredDate, currentDate);
 
-	        // Print result
 	        System.out.println("Duration from entered date to current date:");
 	        System.out.println(duration.getYears() + " years");
 	        System.out.println(duration.getMonths() + " months");
