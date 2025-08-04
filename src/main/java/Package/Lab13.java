d6e4434d34aa8aed9a5c66c70340e3d1bae79bfd
Richa <richasahu4190@gmail.com>
2025-08-04 17:40:05 +0530

    latest commit

---
+++ src/main/java/Package/Lab13.java
@@ -0,0 +1,33 @@
+package Package;
+
+import java.util.Scanner;
+import java.time.LocalDate;
+
+public class Lab13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the purchase date (yyyy-mm-dd): ");
+		String input = sc.nextLine();
+		LocalDate purchaseDate = LocalDate.parse(input);
+		
+		System.out.print("Enter warranty period in years: ");
+		int warrantyYears = sc.nextInt();
+		
+		System.out.print("Enter warranty period in months: ");
+		int warrantyMonths =  sc.nextInt();
+		
+		
+		LocalDate expiry = calculate(purchaseDate, warrantyYears, warrantyMonths);
+		
+		System.out.print("Warranty expires on :" + expiry);
+	}
+		public static LocalDate calculate(LocalDate purchaseDate , int years, int months) {
+			return purchaseDate.plusYears(years).plusMonths(months);
+		}
+
+	}
+
+
