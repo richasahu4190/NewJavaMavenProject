be0eaf49c8366869e9ab39f2457cc2b609f26076
Richa <richasahu4190@gmail.com>
2025-08-06 16:37:45 +0530

    new1

---
+++ src/main/java/Aug05/com/cg/eis/service/EmployeeService.java
@@ -0,0 +1,8 @@
+package Aug05.com.cg.eis.service;
+
+import com.cg.eis.bean.Employee;
+
+public interface EmployeeService {
+    String assignInsuranceScheme(Employee emp);
+    void displayEmployeeDetails(Employee emp);
+}
